fa545dbce6365cef68f62fdbb59c4ee13edde77b
Muhammad Zunair <muhammad.zunair@xprolabs.com>
2022-03-17 03:56:56 -0700

    welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "welcome#index"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :welcomes, only:%i[index]
 end
